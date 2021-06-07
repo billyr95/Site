d83bd35d3d31b5acac90a47aaf2a3acf917a3ad6
billyr95 <60150079+billyr95@users.noreply.github.com>
2021-06-07 12:16:36 -0400

    Code Push
    First Batch of Code Brought in

---
+++ JS/liquidimage.js
@@ -0,0 +1,7 @@
+var hoverDistort = new hoverEffect({
+    parent: document.querySelector('.wrapper'),
+    intensity: 0.5,
+    image1: 'images/Wyoming09.png',
+    image2: 'images/Wyoming14.png',
+    displacementImage: 'images/DistortionMat.png'
+});
